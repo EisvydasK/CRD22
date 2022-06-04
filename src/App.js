cd14a9ff0099dca8c81d4e3e2ef10eacd2694f01
https://github.com/EisvydasK <eisvydask@gmail.com>
2022-06-05 00:57:51 +0300

    set state/render app.js

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Eisvydas',
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name}
+          </p>
+          <button 
+            onClick={() => {
+              this.setState({name: 'Babaitunas'})
+            }}
+          >
+             Change name 
+          </button>
+        </header>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
